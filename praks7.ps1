3129465128331a3b98367e5310baf8c9971d4bf8
Intz004 <indrek.uibo@voco.ee>
2025-01-30 00:44:00 +0200

    Praks7 tegin värviliseks

--- praks7.ps1
+++ praks7.ps1
@@ -1,11 +1,18 @@
 $i = 0
 
-$color = @("Red", "Green", "Yellow", "Blue")
+$color = @("Red", "Green", "Yellow", "Cyan")
 
 while($i -lt 20){
     $random = Get-Random -Maximum 4
     $i++
-    
     $data = "$i $($color[$random])"
-    Write-Host $data
+
+    switch($random) {
+        0 {Write-Host $data -ForegroundColor Red}
+        1 {Write-Host $data -ForegroundColor Green}
+        2 {Write-Host $data -ForegroundColor Yellow}
+        3 {Write-Host $data -ForegroundColor Cyan}
+        Default {Write-Host "No Value"}
+    }    
+
 }
