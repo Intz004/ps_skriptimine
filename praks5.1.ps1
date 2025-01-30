cd95c62e71da7c7fdc043cf8968c4f0ea9b2c8e5
Intz004 <indrek.uibo@voco.ee>
2025-01-30 22:14:24 +0200

    praks5

---
+++ praks5.1.ps1
@@ -0,0 +1,12 @@
+﻿$process = Get-Process | where{$_.ProcessName -eq "notepad"} | Select ProcessName, Id 
+
+
+$dir = Dir C:\Temp
+ 
+
+Write-Host "Filename: $($dir.Name)"
+Write-Host "Size in KB: $($dir.Length/1kB)"
+Write-Host "Size in MB: $($dir.Length/1MB)"
+
+
+$process , $dir | Export-Csv -Path "C:\Temp\test.csv" -NoTypeInformation
