9a154950089aa8ebbc069634b4ea9f35b4a433d0
Intz004 <indrek.uibo@voco.ee>
2025-01-30 23:46:35 +0200

    praks12

---
+++ praks12.ps1
@@ -0,0 +1,15 @@
+﻿function countstart {
+$kokku = (Get-Service | Where{$_.Status -eq 'Running'}).Count
+    Write-Host "Kokku töötavaid teenuseid hetkel: $($kokku)"}
+
+
+
+function countstop{
+$kokku = (Get-Service | Where{$_.Status -eq 'Stopped'}).Count
+    Write-Host "Kokku seiskunud teenuseid hetkel: $($kokku)"}
+
+function countservice{
+    param([string]$status)
+    $kokku = (Get-Service | Where($_.Status -eq $status))
+    Write-Host "Hetkel on kokku:" $kokku.Count $status "staatuses"
+    }
