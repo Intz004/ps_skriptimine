bcf9ff81878ef7a974e72c0e938895a3611d7f83
Intz004 <indrek.uibo@voco.ee>
2025-01-30 23:22:28 +0200

    praks11 kalkulaator

---
+++ praks11.ps1
@@ -0,0 +1,22 @@
+﻿Write-Host "Sisestage kaks numbrit:"
+[int]$number1 = Read-Host "Sisestage esimene number"  
+return 0
+[int]$number2 = Read-Host "Sisestage teine number" 
+
+Write-Host "Kalkulaator" -ForegroundColor Cyan
+Write-Host "1. Liitmine" -ForegroundColor Magenta
+Write-Host "2. Lahutamine" -ForegroundColor Magenta
+Write-Host "3. Korrutamine" -ForegroundColor Magenta
+Write-Host "4.Jagamine" -ForegroundColor Magenta
+$arvutatav = Read-Host "Tehke oma valik, mida soovite numbritega teha" 
+
+if ( $arvutatav -eq 1) {
+$vastus= [int]$number1 + [int]$number2}
+elseif ( $arvutatav -eq 2) {
+$vastus= [int]$number1 - [int]$number2}
+elseif ( $arvutatav -eq 3) {
+$vastus= [int]$number1 * [int]$number2}
+elseif ( $arvutatav -eq 4) {
+$vastus= [int]$number1 / [int]$number2}
+
+Write-Host "Vastus on: " $vastus
